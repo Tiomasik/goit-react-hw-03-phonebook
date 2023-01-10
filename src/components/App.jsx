db9dbdfeaf46984a95c31184ad70a426dd95c0b0
Tiomasik <a.masliy@ukr.net>
2023-01-10 23:06:58 +0200

    made 1st task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import Filter from './ContactsBook/Filter'
 
 class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
   }
 
@@ -40,6 +35,23 @@ class App extends Component {
   getVisibleContacts = () => {
     const { filter, contacts } = this.state
     return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(contacts)
+
+    if (parsedContacts) {
+      this.setState({contacts: parsedContacts})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts))
+    }
+    
   }
 
 
